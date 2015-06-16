d3d01d633780d84c4cfcd57413a079187375fde2
Esskov Andrey <p_uzo@mail.ru>
2015-06-16 15:18:40 +0300

    sam

--- routes/registration.js
+++ routes/registration.js
@@ -1,16 +1,30 @@
 //      registration
 
-var mongo = require('../public/js/mongoConnect');
-var users = require('../public/js/users').Users;
+// var mongo = require('../public/js/mongoConnect');
+var mongo = require('mongodb').MongoClient;
+//var users = require('../public/js/users').Users;
 
 exports.post = function (req, res) {
-    var name = req.body.regUser,
-        pass = req.body.regPass,
-        mess = 'Извините, пользователь с таким именем уже существует.'
+    var name = req.body.regUser
+        , pass = req.body.regPass
+        , mess = 'Извините, пользователь с таким именем уже существует.'
             + '<br/>'
             +  'Попробуйте придумать новый псевдоним';
     console.log('name=' + JSON.stringify(req.body));
-    var user = users.addUser(name, pass, function (err, pers) {
-        if(err) throw err;
-    })
+    //users.addUser(name, pass, function (err, user) {
+    //    if(err) callback(err, null);
+    //    console.log('user=' + JSON.stringify(user));
+    //    callback(null, user);
+    //
+    //})
+
+    mongo.connect('mongodb://localhost/akuraChat', function (err, db) {
+       if(err) throw err;
+        var col = db.collection('accounts');
+        col.findOne({name: name}, function (err, user) {
+            if(err) throw err;
+            console.log(user);
+
+        });
+    });
 };
