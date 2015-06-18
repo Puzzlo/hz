81a42112015cbb10755b2d811f6704001dcc874f
Esskov Andrey <p_uzo@mail.ru>
2015-06-18 16:29:12 +0300

    reg+log worked smooth

--- routes/login.js
+++ routes/login.js
@@ -9,20 +9,33 @@ exports.post = function (req, res) {
     console.log(' in post login ');
     var name = req.body.loginUser
         , pass = req.body.loginPass;
+    console.log('params in login is: ' + JSON.stringify(req.body));
 
     mongo.connect('mongodb://localhost/akuraChat', function (err, db) {
         if (err) throw err;
         var col = db.collection('accounts');
-        col.findOne({usename: name}, function (err, user) {
+        col.findOne({username: name}, function (err, user) {
+            console.log(JSON.stringify(user));
            if ( err ) res.render('error', {errorText: 'Ошибка при поиске в базе данного логина'});
-            if(!user) {
+            if(user == null) {
                 setTimeout(
                     res.render('error', {errorText: 'Нет такого логина'}),
                 2000);
                 //res.redirect('login');
             } else {
-                console.log(user);
-                res.render('error', {errorText: 'Есть такой логин, добро пожаловать'})
+                //console.log(user);
+                //res.render('error', {errorText: 'Есть такой логин, добро пожаловать'})
+                bcrypt.compare(pass, user.password, function (err, result) {
+                    if ( err ) throw err;
+                    if(result) {
+                        res.redirect('chat');
+                    } else {
+                        res.render('error', {errorText: JSON.stringify(user)});
+                    }
+
+                })
+
+
             }
         });
     });
